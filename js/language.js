c40ea8fc20256b2da07b4fe071f28212a1dbb68d
zhongshuai <zhongshuai@megvii.com>
2018-10-26 14:29:28 +0800

    ong

---
+++ js/language.js
@@ -0,0 +1,118 @@
+var chinese = {
+
+    language:"English",
+
+    logoText:"武汉康帕斯国际物流有限公司",
+
+    menu1: "关于我们",
+    menu2: "公司主营",
+    menu3: "特色业务",
+    menu4: "联系我们",
+
+    banner1:"康帕斯",
+    banner1Detail1:"为你的货物指引方向",
+
+    banner2:"康帕斯",
+    banner1Detail2:"想你所想，行你所行",
+
+    title: "康帕斯",
+    aboutUs: "关于我们",
+    aboutUsDetail: "    康帕斯国际物流，致力于国际货运代理服务。服务包括承办海运，空运，铁运，陆运等国际货物运输代理。包括揽货、订舱、托运、仓储、包装、装卸；报送、报检、保险；国际展品、私人物品、及过境货物代理；咨询业务及其他国际货运代理业务和综合物流业务。"+
+    "<br>    公司将充分发挥整体优势，面向海内外市场，不断开拓经营，通过实施经营规模化、功能多元化和国际化的战略，逐步建成一个规模宏大、实力雄厚、功能齐全、具有市场竞争优势的综合物流公司。 ",
+
+    ourServices:"我们的服务",
+
+    sea: "海运",
+    seaDetail: "集装箱，散杂，滚装",
+
+    air: "空运",
+    airDetail: "客机，货机，包机",
+
+    rail: "铁运",
+    railDetail: "中欧，中亚，中俄",
+
+    road:"内陆运输",
+    roadDetail:"港口，口岸，工厂",
+
+
+    specia: "特色业务",
+    specia1: "港口代理",
+    specia2: "口岸过境",
+    specia3: "中欧班列",
+    specia4: "长江内支线",
+    specia5: "危险品运输",
+    specia6:"超大件, 超重件",
+
+    photo: "相册",
+
+
+    contact:"联系我们",
+    companyName:"武汉康帕斯国际物流有限公司",
+    address:"地址：武汉市汉阳区金龙南路和隆祥街交汇处悦城街区11、12号楼3层",
+    express: "邮寄地址：武汉经开万达广场B4座0713",
+    web: "Web: www.compasafe.com",
+    tel: "Tel: +86 166 0275 8663",
+    email: "Email: info@compasafe.com"
+
+
+}
+
+
+var english = {
+
+    language:"中文",
+
+    logoText:"Compasafe International Logistics Limited",
+
+    menu1: "ABOUT US",
+    menu2: "OUR SERVICE",
+    menu3: "SPECIALIST SERVICE",
+    menu4: "CONTACT US",
+
+
+    banner1:"Compasafe",
+    banner1Detail1:"Compasafe, The safety compass for your cargo",
+
+    banner2:"Compasafe",
+    banner1Detail2:"Do whatever you want，goes wherever he wants ",
+
+    title: "Compasafe",
+    aboutUs: "ABOUT US",
+    aboutUsDetail: "    Compasafe International Logistics Limited, dedicated to international freight forwarding services. Our services include shipping, air, rail, land and other international freight forwarding agents, including cargo, booking, shipping, warehousing, packaging, loading & unloading; reporting, inspection, insurance; international exhibits, private goods, and transit cargo; Consulting business, other international freight forwarding business and integrated logistics business. <br> Our team will give full play to our overall advantages, face the market at home and abroad, and continue to open up our business. Through the implementation of the strategy of large-scale operation, diversification of functions and internationalization, the company will gradually build a large-scale, powerful and well-functioning company. A comprehensive logistics company with a competitive edge in the market.",
+
+    ourServices:"Our Services",
+
+    sea: "SEA FREIGHT",
+    seaDetail: "Containe / Breakbulk / RORO",
+
+    air: "AIR FREIGHT",
+    airDetail: "Airliner / Air freighter / Charter flight",
+
+    rail: "RAIL TRASPORTATION",
+    railDetail: "China—Europe / China—Central Asia / China--Russia",
+
+    road:"ROAD TRASPORTATION",
+    roadDetail:"Port / Border / Factory",
+
+
+    specia: "SPECIALIST SERVICE",
+    specia1: "Port service",
+    specia2: "Border service",
+    specia3: "China Railway Express",
+    specia4: "Inland feeder",
+    specia5: "Dangerous goods",
+    specia6:"Special cargo",
+
+    photo: "Gallery",
+
+
+    contact:"CONTACT US",
+    companyName:"COMPASAFE INTERNATIONAL LOGISTICS LIMITED",
+    address:"Address: No.303-20, 3nd floor, Building 11,12 Yuecheng Square, Cross of Jinlong South Road and Longxiang Street,Hanyang District, Wuhan City, Hubei Province, China",
+    express: "Express address: No.0713, Block B4, Wanda Square, Economic Development Zone, Wuhan, Hubei, China",
+    web: "Web: www.compasafe.com",
+    tel: "Tel: +86 166 0275 8663",
+    email: "Email: info@compasafe.com"
+
+
+}
